1f01998c5b70a9c5281ca9aba1615ec99d4474b6
ElG87 <115730576+ElG87@users.noreply.github.com>
2023-08-12 20:15:28 +0200

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-$('#preview-blog1').load('archief.html #blog-1');
-
-$('#knop-blog-1').on('click', function(){
 
 $('html/detailpagina.html #huidige-blog').load('archief.html #blog-1');    
-})
 });
