304a601769010c5bec289ccd0128079c0bbbab5a
ElG87 <115730576+ElG87@users.noreply.github.com>
2023-08-12 20:01:17 +0200

    update blog

--- js/blog.js
+++ js/blog.js
@@ -3,6 +3,6 @@ $('#preview-blog1').load('archief.html #blog-1');
 
 $('#knop-blog-1').on('click', function(){
 
-$('detailpagina.html #huidige-blog').load('archief.html #blog-1');    
+$('html/detailpagina.html #huidige-blog').load('archief.html #blog-1');    
 })
 });
