40b8396d3b3ea29530e055bac8d24aa514761a60
ElG87 <115730576+ElG87@users.noreply.github.com>
2023-08-13 23:10:58 +0200

    update blog

--- js/blog.js
+++ js/blog.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
-    $('#knop-blog1').on('click', function () {
-        $('#preview-blog1').load('archief.html #blog-1',() =>{
-                    $('#divResult').html('Blog 1 succesvol geladen');
-                    if('#divResult' = success){
-                        alert('succesvol')
-                    };
+
+    $('#preview-blog1').load('archief.html #blog-1 #img-blog1, #eerste-p, #date1');
+ 
+        $('#knop-blog-1').on('click', function(){
+            window.location.href='detailpagina.html';
         });
-    });
+    
+    $('#huidige-blog').load('archief.html #blog-1');
+   
+    $('#preview-blog2').load('archief.html #blog-2 #img-blog2, #tweede-p , #date2');
+    $('#preview-blog3').load('archief.html #blog-3 #img-blog3, #derde-p , #date3');
+
+
 });
