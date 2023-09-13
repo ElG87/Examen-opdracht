0026c6a86c106e77fa5ed75be3642820722ab515
ElG87 <115730576+ElG87@users.noreply.github.com>
2023-09-13 23:50:43 +0200

    Update blog.js
    Blog 2 aangepast

--- js/blog.js
+++ js/blog.js
@@ -6,21 +6,20 @@ $(document).ready(function() {
             window.location.href='detailpagina.html';
         });
     
-    $('#huidige-blog').load('archief.html #blog-1' , () =>{
-        $(#divAlert).html('succesvol geladen');
+    $('#huidige-blog').load('archief.html #blog-1');
+});
 
-   $.ajax({
-        url: 'detailpagina.html',
-        succes: function(data){
-            $('#preview-blog2').load
-                ('archief.html #blog-2 #img-blog2, #tweede-p , #date2');
-                $('#knop-blog-2').on('click', function(){
-                    window.location.href='detailpagina.html';
-                });
-        
-        $('#huidige-blog').load('archief.html #blog-2');
-        }
+        $('#knop-blog-2').on('click', function(){
+        $.ajax({
+            url: 'archief.html #blog-1',
+            succes: function(data){
+                $('#huidige-blog').html(data);
+         },
+            error: function(data){
+              $('#huidige-blog').html('Er is iets fout gegaan!')
+             }
         });
+    });
     $('#preview-blog3').load('archief.html #blog-3 #img-blog3, #derde-p , #date3');
 
 
